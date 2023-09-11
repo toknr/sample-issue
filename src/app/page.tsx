0023b9dd3f4731bfe51a2e74e6693ff5c62ca781
tokinari.yasuda <tokinari.yasuda@gmail.com>
2023-09-11 17:50:01 +0900

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{"a "}
+            By{"test3-2"}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
